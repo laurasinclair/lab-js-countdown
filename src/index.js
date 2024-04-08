dca0fde444802d6fcffff0d3653f2bca20e43aea
Laura Sinclair <l.sinclair@tuta.io>
2024-04-08 16:26:57 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,24 +1,56 @@
+// HTML elements
+const divTime = document.getElementById('time')
+const toastCard = document.getElementById('toast');
+const startBtn = document.getElementById('start-btn')
+const closeToast = document.getElementById('close-toast')
+const toastMsg = document.getElementById('toast-message')
+
+// Useful variables
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
+divTime.innerHTML = DURATION;
 
 
 // ITERATION 1: Add event listener to the start button
-
-// Your code goes here ...
-
-
+startBtn.addEventListener('click', () => {
+  startCountdown();
+})
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
+  if (remainingTime !== DURATION) {
+    remainingTime = DURATION;
+  }
 
-  // Your code goes here ...
+  timer = setInterval(test, 1000)
+  
+  function test() {
+    if (remainingTime <= 0) {
+      clearInterval(timer); 
+    } else {
+      remainingTime -= 1;
+      divTime.innerHTML = remainingTime;
+
+      if (remainingTime === 10) {
+        showToast('⏰ Final countdown! ⏰')
+      } 
+
+      if (remainingTime === 5) {
+        showToast('Start the engines! 💥')
+      } 
+
+      if (remainingTime <= 0) {
+        showToast('Lift off! 🚀')
+      }
+    }
+  }
+  showToast();
 }
-
 
 
 
@@ -26,13 +58,19 @@ function startCountdown() {
 function showToast(message) {
   console.log("showToast called!");
 
-  // Your code goes here ...
+  if (message) {
+    toastMsg.innerHTML = message
+  }
 
+  toastCard.classList.add('show');
 
+  setTimeout(() => {
+    toastCard.classList.remove('show');
+  }, 3000)
 
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+  closeToast.addEventListener('click', () => {
+    toastCard.classList.remove('show');
+    clearInterval(timer);
+    startCountdown();
+  })
 }
